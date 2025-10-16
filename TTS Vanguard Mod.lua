3e63d1157130af2e6b23faadbcf2f4c6f0bb6650
SonitekProg <69764966+SonitekProg@users.noreply.github.com>
2025-10-15 23:04:52 -0400

    Added the First Block for Nubatama

--- TTS Vanguard Mod.lua
+++ TTS Vanguard Mod.lua
@@ -1,7 +1,9 @@
 function onLoad()
 	MasterURL = "https://raw.githubusercontent.com/SonitekProg/Vanguard-TTS-Mod-Files/refs/heads/main/VTTSMasterFile.json"
-	CustomDecksURL = ""
+	CustomDecksURL = "https://raw.githubusercontent.com/SonitekProg/Vanguard-TTS-Mod-Files/refs/heads/main/CustomDecks.json"
 	MasterFile = {}
+	CustomDecks = {}
+	CurrentLoadList = {}
 	Wait.time(LoadJSON, 0.1)
 end
 
@@ -9,14 +11,25 @@ function LoadJSON()
 	WebRequest.get(MasterURL,	function(response)
 								if (response.is_error)
 								then
-									broadcastToAll("Core JSON File failed to load. Contact Mod creator with error code: LIBFAIL.", {1,0,0})
+									broadcastToAll("Core JSON File failed to load. Contact Mod creator with error code: LIBFAILJ", {1,0,0})
 									broadcastToAll("Mod will not funciton properly.", {1,0,0})
 									return
 								end
-								JSON.decode(response.text)
-								
-								MasterFile = result
-								broadcastToAll("Mod fully loaded, and can now be used properly.")
+								MasterFile = JSON.decode(response.text)
 								end)
+	WebRequest.get(CustomDecksURL,	function(response)
+									if (response.is_error)
+									then
+										broadcastToAll("Information for Custom Decks has failed to load. Contact Mod creator with error code: CUSTFAILJ", {1,0,0})
+										broadcastToAll("Mod will not funciton properly.", {1,0,0})
+										return
+									end
+									CustomDecks = JSON.decode(response.text)
+									end)
+	broadcastToAll("Mod fully loaded, and can now be used properly.")
 	return 1
+end
+
+function LoadNext()
+	
 end
