7c310af57f1a05b6db8352d327629ea33be261d7
SonitekProg <69764966+SonitekProg@users.noreply.github.com>
2025-10-15 22:30:47 -0400

    Re-adjusted the formatting of the MasterFile

--- TTS Vanguard Mod.lua
+++ TTS Vanguard Mod.lua
@@ -6,20 +6,13 @@ end
 
 function LoadJSON()
 	WebRequest.get(MasterURL,	function(response)
-								broadcastToAll("Initiating WebRequest.")
 								if (response.is_error)
 								then
 									broadcastToAll("Core JSON File failed to load. Contact Mod creator with error code: LIBFAIL.", {1,0,0})
 									broadcastToAll("Mod will not funciton properly.", {1,0,0})
 									return
 								end
-								broadcastToAll("JSON File accessed properly.")
-								local success, result = pcall(JSON.decode, response.text)
-								
-								if not success then
-									broadcastToAll("JSON Decoding has failed. Error in JSON format or decoder.", {1,0,0})
-									return
-								end
+								JSON.decode(response.text)
 								
 								MasterFile = result
 								broadcastToAll("Mod fully loaded, and can now be used properly.")
